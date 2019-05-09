1572b0296f6fcae5e5be8981bd275e4bdcce36e8
João Vitor <34252083+vitornic@users.noreply.github.com>
2019-05-09 14:34:15 -0300

    hrefs

--- php/verifica.php
+++ php/verifica.php
@@ -15,7 +15,7 @@ $Cidade         = $_POST['Cidade'];
 $Estado         = $_POST['Estado'];
 $DataNasc       = $_POST['DataNasc'];
 $erro			= 0;
-
+/*
 if (empty($Nome) OR strstr($Nome,' ') == true) {
 	echo "Favor digitar o seu nome corretamente.<br>";
 	$erro = 1;
@@ -30,7 +30,7 @@ if (strlen($Estado)!=2) {
 	echo "Favor digitar o seu estado corretamente.<br>";
 	$erro = 1;
 }
-
+*/
 if ($erro == 0) {
 	echo "Todos os dados foram digitados corretamente.<br>";
 	include '../inc/insere.inc';
